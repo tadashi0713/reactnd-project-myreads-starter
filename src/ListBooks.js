fb81fde57883dbc5c557dc18fb4bcde8b76f0146
根本征 <tadashi.nemoto0713@gmail.com>
2017-08-27 03:12:09 +0900

    Add link for search

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from  'react-router-dom'
 import PropTypes from 'prop-types'
 import Book from './Book'
 
@@ -35,7 +36,7 @@ class ListBooks extends Component {
               <div className="bookshelf-books">
                 <ol className="books-grid">
                   {currentlyReadingBooks.map((book) => (
-                    <Book book={book}/>
+                    <Book book={book} key={book.title}/>
                   ))}
                 </ol>
               </div>
@@ -45,7 +46,7 @@ class ListBooks extends Component {
               <div className="bookshelf-books">
                 <ol className="books-grid">
                   {wantToReadBooks.map((book) => (
-                    <Book book={book}/>
+                    <Book book={book} key={book.title}/>
                   ))}
                 </ol>
               </div>
@@ -55,7 +56,7 @@ class ListBooks extends Component {
               <div className="bookshelf-books">
                 <ol className="books-grid">
                   {readBooks.map((book) => (
-                    <Book book={book}/>
+                    <Book book={book} key={book.title}/>
                   ))}
                 </ol>
               </div>
@@ -63,7 +64,7 @@ class ListBooks extends Component {
           </div>
         </div>
         <div className="open-search">
-          <a>Add a book</a>
+          <Link to="/search">Add a book</Link>
         </div>
       </div>
     )
