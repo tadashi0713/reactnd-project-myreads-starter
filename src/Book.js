1ae55232774b3fbe07bc1f553edf258ed40a79c3
tadashi0713 <tadashi.nemoto0713@gmail.com>
2017-09-05 08:39:14 +0900

    Remove style in css

--- src/Book.js
+++ src/Book.js
@@ -14,7 +14,7 @@ class Book extends Component {
       <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }} />
+            <div className="book-cover" style={{ backgroundImage: `url(${book.imageLinks.smallThumbnail})` }} />
             <div className="book-shelf-changer">
               <select
                 value={book.shelf || 'none'}
